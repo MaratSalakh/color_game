ee1e32c62418e08a518cf6332ea136cd4888f465
Marat Salakh <marat.salakh.dev@gmail.com>
2024-04-11 16:46:43 +0200

    add SelectStandart.vue

---
+++ src/components/UI/SelectStandart.vue
@@ -0,0 +1,28 @@
+<template>
+  <select :value="modelValue" @change="changeOption">
+    <option disabled value="">Select from list</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  name: "SelectStandart",
+  props: {
+    modelValue: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    changeOption(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
+<style></style>
