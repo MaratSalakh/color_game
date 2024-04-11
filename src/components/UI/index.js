5d60b324f4eed6172601b0aeb1dd8f43b8d4b57b
Marat Salakh <marat.salakh.dev@gmail.com>
2024-04-11 16:47:23 +0200

    register SelectStandart in app

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,5 +1,6 @@
 import ButtonStandart from "./ButtonStandart.vue";
 import InputStandart from "./InputStandart.vue";
 import DialogStandart from "./DialogStandart.vue";
+import SelectStandart from "./SelectStandart.vue";
 // in this file we export all components for registration
-export default [ButtonStandart, InputStandart, DialogStandart];
+export default [ButtonStandart, InputStandart, DialogStandart, SelectStandart];
