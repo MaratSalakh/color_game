0615cc475babda9f5b37cd6053122e7241c0c35c
Marat Salakh <marat.salakh.dev@gmail.com>
2024-04-18 12:26:19 +0200

    add products components

---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card" style="width: 15rem; height: 15rem">
+    <img src="" class="card-img-top" alt="" />
+    <div class="card-body">
+      <p class="card-text">
+        {{ product.name }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/products/ProductsSlider.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row row-cols-4">
+    <div v-for="product in products" :key="product.id">
+      <ProductsCard class="my-2" :product="product"></ProductsCard>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductsCard from "./ProductCard.vue";
+
+export default {
+  components: { ProductsCard },
+  props: {
+    products: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
