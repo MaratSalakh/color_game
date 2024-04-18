92b471e6f4d9eb93b54f5ca1b01ece25f2dfc964
Marat Salakh <marat.salakh.dev@gmail.com>
2024-04-18 12:26:37 +0200

    refactoring mainPage

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,12 +1,54 @@
 <template>
   <NavBar currentPage="home"></NavBar>
+  <div class="container">
+    <div class="row my-2">
+      <div class="col-3">
+        <SelectStandart
+          :modelValue="selectedSort"
+          @update:modelValue="setSelectedSort"
+          :options="sortOptions"
+        ></SelectStandart>
+      </div>
+      <div>
+        <ProductsSlider :products="sortedAndSearchedProducts"></ProductsSlider>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import ProductsSlider from "@/components/products/ProductsSlider.vue";
+
+import { mapActions, mapGetters, mapState, mapMutations } from "vuex";
+
 export default {
   components: {
     NavBar,
+    ProductsSlider,
+  },
+  methods: {
+    ...mapMutations({
+      setSelectedSort: "main/setSelectedSort",
+    }),
+    ...mapActions({
+      fetchProducts: "main/fetchProducts",
+    }),
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  computed: {
+    ...mapState({
+      isLoadingData: (state) => state.main.isLoadingData,
+      selectedSort: (state) => state.main.selectedSort,
+      searchQuery: (state) => state.main.searchQuery,
+      products: (state) => state.main.products,
+      sortOptions: (state) => state.main.sortOptions,
+    }),
+    ...mapGetters({
+      sortedAndSearchedProducts: "main/sortedAndSearchedProducts",
+    }),
   },
 };
 </script>
