4145fe652665613eb3afdfb8209fad99fb4f31d7
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-04 13:23:16 +0200

    back changes

--- src/components/products/ProductCard.vue
+++ src/components/products/ProductCard.vue
@@ -2,8 +2,7 @@
   <div class="card" style="width: 15rem; height: 15rem">
     <div class="card-body">
       <h5 class="card-title">{{ product.name }}</h5>
-      <label for="customRange1" class="form-label">Select speed</label>
-      <input type="range" class="form-range" id="animationSpeed" />
+      <span class="card-text">{{ product.description }}</span>
       <div class="d-flex justify-content-center align-items-center h-50">
         <div class="animation"></div>
       </div>
@@ -12,7 +11,7 @@
 </template>
 
 <script>
-import gsap from "gsap";
+import { mapState } from "vuex";
 
 export default {
   props: {
@@ -21,26 +20,17 @@ export default {
       required: true,
     },
   },
-  methods: {},
-  mounted() {
-    gsap.to(".animation", {
-      duration: 2,
-      scale: 2,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.out",
-      yPercent: 50,
-    });
+  computed: {
+    ...mapState({
+      products: (state) => state.main.products,
+    }),
+  },
+  watch: {
+    products() {
+      this.changeAnimation();
+    },
   },
 };
 </script>
 
-<style lang="css" scoped>
-.animation {
-  margin: 10px;
-  width: 50px;
-  height: 50px;
-  background-color: red;
-  border-radius: 100%;
-}
-</style>
+<style lang="css" scoped></style>
